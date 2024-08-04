607cebfab6854832e160ccc059575072a748a4dc
zhangziqi <1163070713@qq.com>
2024-08-04 22:01:06 +0800

    添加 try with source lambda表达式 和 List.foreach 注释

--- demo/src/main/java/com/example/filereader/controller/docController.java
+++ demo/src/main/java/com/example/filereader/controller/docController.java
@@ -15,8 +15,12 @@ public class docController {
 
     @GetMapping("/read")
     public String read(String filePath) throws IOException {
+        // try with source 在执行完代码块内代码后自动关闭资源
         try (FileInputStream fis = new FileInputStream(filePath);
              XWPFDocument document = new XWPFDocument(fis)) {
+            //lambda表达式简化函数式接口
+            //这里默认实现的式accept方法
+            //这里式 List.foreach方法注意不要和 stream 的foreach混淆
             document.getParagraphs().forEach(paragraph ->
                     System.out.println(paragraph.getText()));
         }
